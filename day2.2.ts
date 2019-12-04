6506986a9e5de38201e00e3d7f9ff0fa1f844e06
harrygr <harry@bulb.co.uk>
2019-12-04 14:58:22 +0000

    Day2.2

--- day2.2.ts
+++ day2.2.ts
@@ -4,7 +4,6 @@ import fs from "fs";
 const restoreTo1202ProgramAlarm = R.compose(R.update(2, 2), R.update(1, 12));
 
 const processInput = R.compose(
-  restoreTo1202ProgramAlarm,
   R.filter(Number.isFinite),
   R.map(parseFloat),
   R.split(","),
@@ -13,11 +12,8 @@ const processInput = R.compose(
 );
 
 const opcodes = processInput(fs.readFileSync("day2.input.txt"));
-
-const opcodeWithIndex: [number, number][] = opcodes.map((val, idx) => [
-  val,
-  idx
-]);
+const addIndex = (codes: number[]) =>
+  codes.map<[number, number]>((val, i) => [val, i]);
 
 const isOperation = R.compose(R.equals(0), R.modulo(R.__, 4));
 
@@ -36,11 +32,11 @@ const runIntcode = R.compose(R.map(R.head), (codes: [number, number][]) =>
     (_, elem) => R.not(isEnd(elem)),
     (acc, [val, idx]) => {
       if (isOperation(idx)) {
-        const firstVal = valAtIndex(R.add(1, idx), acc);
-
-        const secondVal = valAtIndex(R.add(2, idx), acc);
         const outputIdx = valAt(R.add(3, idx), acc);
-        const newVal = (val === 1 ? R.add : R.multiply)(firstVal, secondVal);
+        const newVal = (val === 1 ? R.add : R.multiply)(
+          valAtIndex(R.add(1, idx), acc),
+          valAtIndex(R.add(2, idx), acc)
+        );
 
         return R.update(outputIdx, [newVal, outputIdx], acc);
       }
@@ -51,6 +47,29 @@ const runIntcode = R.compose(R.map(R.head), (codes: [number, number][]) =>
   )
 );
 
-console.log(opcodes);
-const ans = runIntcode(opcodeWithIndex);
+const target = 19690720;
+
+const testValues = R.range(0, 100);
+
+const ans = R.reduceWhile(
+  R.compose(R.not, R.equals(target), R.head),
+  (_, [x, y]) => [
+    R.compose(
+      R.head,
+      runIntcode,
+      addIndex,
+      R.update(2, x),
+      R.update(1, y)
+    )(opcodes),
+    y,
+    x
+  ],
+  [0, 0, 0]
+)(
+  R.compose(
+    R.unnest,
+    R.map((x: number) => R.map(y => [x, y] as const, testValues))
+  )(testValues)
+);
+
 console.log(ans);
