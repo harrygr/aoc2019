eb7f9de2acc39b0799e35b834a03f40a50cdb315
harrygr <harry@grumbar.me>
2019-12-09 20:32:58 +0000

    day5.2

--- day5.ts
+++ day5.ts
@@ -14,7 +14,7 @@ type Machine = {
 
 type MachineWriter = Writer<number[], Machine>;
 
-const INPUT = 1;
+const INPUT = 5;
 
 const lookup = R.nth;
 
@@ -95,14 +95,54 @@ const perform = (
         memory: indirectInsert(ip + 1, INPUT, memory),
         ip: ip + 2
       });
-    case 4:
+    case 4: {
       const parameterModes = getParameterModes(1)(modes);
 
       return Writer([getMemoryValue(parameterModes[0])(ip + 1, memory)], {
         memory,
         ip: ip + 2
       });
+    }
+    case 5: {
+      const parameterModes = getParameterModes(2)(modes);
+      const param1 = getMemoryValue(parameterModes[0])(ip + 1, memory);
+      const param2 = getMemoryValue(parameterModes[1])(ip + 2, memory);
 
+      return Writer([], {
+        memory,
+        ip: R.equals(param1, 0) ? ip + 3 : param2
+      });
+    }
+    case 6: {
+      const parameterModes = getParameterModes(2)(modes);
+      const param1 = getMemoryValue(parameterModes[0])(ip + 1, memory);
+      const param2 = getMemoryValue(parameterModes[1])(ip + 2, memory);
+
+      return Writer([], {
+        memory,
+        ip: R.equals(param1, 0) ? param2 : ip + 3
+      });
+    }
+    case 7: {
+      const parameterModes = getParameterModes(3)(modes);
+      const param1 = getMemoryValue(parameterModes[0])(ip + 1, memory);
+      const param2 = getMemoryValue(parameterModes[1])(ip + 2, memory);
+
+      return Writer([], {
+        memory: indirectInsert(ip + 3, param1 < param2 ? 1 : 0, memory),
+        ip: ip + 4
+      });
+    }
+    case 8: {
+      const parameterModes = getParameterModes(3)(modes);
+      const param1 = getMemoryValue(parameterModes[0])(ip + 1, memory);
+      const param2 = getMemoryValue(parameterModes[1])(ip + 2, memory);
+
+      return Writer([], {
+        memory: indirectInsert(ip + 3, param1 === param2 ? 1 : 0, memory),
+        ip: ip + 4
+      });
+    }
     default:
       throw new Error(`Invalid opcode: ${opcode}`);
   }
@@ -110,7 +150,10 @@ const perform = (
 
 const program = fs.readFileSync("day5.input.txt").toString();
 // const program = "3,0,4,0,99";
-// const program = "1101,100,-1,4,0";
+// const program = "3,9,8,9,10,9,4,9,99,-1,8"; // input equal to 8 (pos)
+// const program = "3,9,7,9,10,9,4,9,99,-1,8"; // input less than 8 (pos)
+// const program = "3,3,1108,-1,8,3,4,3,99"; // input equal to 8 (im)
+// const program = "3,3,1107,-1,8,3,4,3,99"; // input less than 8 (im)
 
 const mem = R.map(parseFloat, R.split(",", program));
 
